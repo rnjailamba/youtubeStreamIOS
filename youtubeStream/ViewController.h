35568fcc312422c82920f93f26272c73b6609a2b
rnjai <rnjailamba12@gmail.com>
2016-11-12 15:50:14 +0530

    first commit

---
+++ youtubeStream/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  youtubeStream
+//
+//  Created by Mr Ruby on 12/11/16.
+//  Copyright © 2016 Rnjai Lamba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ youtubeStream/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  youtubeStream
+//
+//  Created by Mr Ruby on 12/11/16.
+//  Copyright © 2016 Rnjai Lamba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
